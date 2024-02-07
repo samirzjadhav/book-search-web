6d6b7f4d662ac8ad0a6036a4206d9b8609c77248
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-07 13:19:06 +0530

    feat: added component section

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 import { BrowserRouter, Routes } from "react-router-dom";
 // Route add next time
@@ -10,13 +8,6 @@ import { BrowserRouter, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes>
-      <App />
-    </Routes>
+    <Routes></Routes>
   </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
