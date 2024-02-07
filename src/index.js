045fce271281e9170e207458bae0a7a0c4e416bc
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-07 13:35:56 +0530

    feat: added files for folder and router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
-import { BrowserRouter, Routes } from "react-router-dom";
-// Route add next time
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home/Home.js";
+import About from "./Pages/About/About.js";
+import BookList from "./Components/BookList/BookList.js";
+import BookDetails from "./Components/BookDetails/BookDetails.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <BrowserRouter>
-    <Routes></Routes>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="book" element={<BookList />} />
+      <Route path="book/:id" element={<BookDetails />} />
+    </Routes>
   </BrowserRouter>
 );
