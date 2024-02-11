2f931b10c94ee1e0362fc160a877c4873cfe8d97
Samir Jadhav <samirzjadhav@gmail.com>
2024-02-11 21:28:31 +0530

    feat: make search bar script

--- src/Components/SearchForm/SearchForm.js
+++ src/Components/SearchForm/SearchForm.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
+import { FaSearch } from "react-icons/fa";
+import { useNavigate } from "react-router-dom";
+import "./SearchForm.css";
 
 const SearchForm = () => {
   return (
-    <div>
-      <h1>SearchForm</h1>
+    <div className="search-form">
+      <div className="container">
+        <div className="search-form-content">
+          <form action="" className="search-form">
+            <div className="search-form-elem flex items-center justify-between bg-white">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="The Lost World..."
+              />
+              <button className="flex item-center justify-center">
+                <FaSearch className="text-purple-500" size={32} />
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
